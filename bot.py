4727725aee34f963b183e2b01735172c0c14172a
LetsPurge <fn.twilight@gmail.com>
2025-04-30 02:54:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ async def start(update, context):
     await update.message.reply_text("سلام، من زنده‌ام!")
 
 def main():
-    app = Application.builder().token("Y7899798019:AAHHLV6JTMlaLx_iWPXC-ltffu8is09w7hk").build()
+    app = Application.builder().token("7899798019:AAHHLV6JTMlaLx_iWPXC-ltffu8is09w7hk").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
